3569e4ab5eb21a65e28e675d893062114c6bc1da
Rajesh D <dhrajesh1@gmail.com>
2017-11-22 16:44:23 +0530

    cards
    cards

--- src/components/RecipeList.js
+++ src/components/RecipeList.js
@@ -11,7 +11,25 @@ class RecipeList extends React.Component {
     console.log(this.props);
     return(
       <div className='recipeListOuter'>
-        hh
+        <div className='container'>
+          <div className='row'>
+            {
+              this.props.recipes.map((recipe,index) => {
+                return <div className="col-sm-1 col-6 col-md-4" key={index}>
+                  <div className="card">
+                    <img className="card-img-top" src="..." alt="Card image cap" />
+                    <div className="card-block">
+                      <h4 className="card-title">Card title</h4>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                  </div>
+
+                </div>
+              })
+            }
+          </div>
+        </div>
       </div>
     )
   }
